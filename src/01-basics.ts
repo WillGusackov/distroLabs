40c5ecabb5c32a4f42f67956a99e791755c13f25
Will Gusackov <willgusackova@gmail.com>
2026-09-14 13:20:16 +0100

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -5,6 +5,14 @@ interface Friend {
     phone: string;
     age: number
 }
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+        email: string;
+        extension: number;
+    }
+}
 
 const friend1 = {
   name: "Paul Fleming",
